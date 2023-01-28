7bdbc1945baf9f120ae522ff6c305251b89a82c3
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2023-01-28 17:39:47 +0800

    update dropdown toggle size

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -3,7 +3,7 @@ import { Bars3Icon } from "@heroicons/vue/24/solid";
 </script>
 <template>
   <div class="dropdown navbar-item">
-    <label tabindex="0"><Bars3Icon class="h-6 text-slate-1000" /></label>
+    <label tabindex="0"><Bars3Icon class="h-12 w-12 text-purple-1000" /></label>
     <div class="dropdown-menu">
       <slot />
     </div>
