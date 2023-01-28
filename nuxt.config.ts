8569619469fe35597cd71e32f41480f3c62615aa
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2023-01-28 17:11:51 +0800

    update open graph info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,17 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Robert Latamaosadi",
+      title: "Robert Latamaosadi - Frontend Developer",
       meta: [
         {
           name: "description",
           content:
             "I'm a UX-oriented frontend developer specializing in building high-quality digital products. Currently a part of amazing people enabling exceptional digital financial experience at Brankas.",
+        },
+        {
+          name: "og:image",
+          content:
+            "https://res.cloudinary.com/latamaosadi-com/image/upload/v1674896185/og-image.jpg",
         },
       ],
     },
@@ -44,6 +49,15 @@ export default defineNuxtConfig({
         "UX-oriented frontend developer specializing in building high-quality digital products",
       theme_color: "#0C192E",
       background_color: "#0d0c1d",
+    },
+    meta: {
+      ogSiteName: "latamaosadi.vercel.app",
+      ogTitle: "Robert Latamaosadi - Personal Website",
+      ogDescription:
+        "UX-oriented frontend developer specializing in building high-quality digital products",
+      ogImage:
+        "https://res.cloudinary.com/latamaosadi-com/image/upload/v1674896185/og-image.jpg",
+      twitterCard: "summary_large_image",
     },
   },
   delayHydration: {
