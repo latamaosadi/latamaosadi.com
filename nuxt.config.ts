7ee4c394b57d587764dbc3c5e6b9ece6c3d09129
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2023-01-31 03:18:50 +0800

    change hydration mode to manual

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -92,7 +92,7 @@ export default defineNuxtConfig({
   delayHydration: {
     // enables nuxt-delay-hydration in dev mode for testing
     debug: process.env.NODE_ENV === "development",
-    mode: "mount",
+    mode: "manual",
   },
   plugins: [
     // {
